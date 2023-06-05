c5db6155bfc07f2b7409c439313a746c785c1961
subash-Ash <73168338+subash-Ash@users.noreply.github.com>
2023-06-05 15:28:43 +0530

    added style

--- app.js
+++ app.js
@@ -6,18 +6,19 @@ let ok_button=document.querySelector("#ok_button");
 let winner = document.querySelector("#winner");
 let round_count = 1;
 let teamA_name = document.querySelector("#teamA_name");
-let teamB_name = document.querySelector("#teamB_name")
+let teamB_name = document.querySelector("#teamB_name");
 
 let submit = ok_button.addEventListener("click",function(event){
     let round_row = document.querySelector("#round_row");
     let teamA_point_row = document.querySelector("#teamA_point_row");
     let teamB_point_row = document.querySelector("#teamB_point_row");
-    round_row.insertAdjacentHTML("afterend",
-    `<td>Round ${round_count}</td>`);
-    teamA_point_row.insertAdjacentHTML("afterend",
-                `<td>${document.querySelector("#teamA_score").value}</td>`);
-    teamB_point_row.insertAdjacentHTML("afterend",
-                `<td>${document.querySelector("#teamB_score").value}</td>`);
+    let score_table_id = document.querySelector("#score_table_id");
+    let tr = document.createElement('tr');
+    let td = document.createElement('td');
+    tr.innerHTML=`<td>Round ${round_count}</td>
+    <td>${document.querySelector("#teamA_score").value}</td>
+    <td>${document.querySelector("#teamB_score").value}</td>`
+    score_table_id.appendChild(tr);
     round_count++;
     teamA_score+=parseInt(document.querySelector("#teamA_score").value);
     teamB_score+=parseInt(document.querySelector("#teamB_score").value);
@@ -37,7 +38,8 @@ let submit = ok_button.addEventListener("click",function(event){
             winner.innerText=teamB_name.value;
         }
     }
-        console.log("match over1");
+        document.querySelector("#teamA_score").value="";
+        document.querySelector("#teamB_score").value="";
         teamA_score_display.innerText = teamA_score;
         teamB_score_display.innerText = teamB_score;
      }
@@ -58,11 +60,9 @@ let score_table=document.querySelector("#score_table");
 score_table.style.display = "none";
 show_score_table.addEventListener("click",function(event){
     if (score_table.style.display === "none") {
-        console.log("1");
         score_table.style.display = "block";
         show_score_table.value="Hide Table"
       } else {
-        console.log("2d");
         score_table.style.display = "none";
         show_score_table.value="Show Table"
       }
